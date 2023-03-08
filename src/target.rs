41d7ab235b7c60d22cd4f1f651e8f1ef84f6bfd0
Robert Zieba <robert@zieba.site>
2023-03-07 22:03:14 -0700

    Target improvements
    - `all` function to create a target with all extensions present
    - Target string parser tests

--- src/target.rs
+++ src/target.rs
@@ -2,11 +2,12 @@ use std::collections::HashSet;
 
 use bibe_instr::BinOp;
 
-#[derive(Clone, Copy, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
 pub enum Extension {
 	IntegerMultplication,
 }
 
+#[derive(Debug)]
 pub struct Target {
 	extensions: HashSet<Extension>,
 }
@@ -18,16 +19,24 @@ impl Target {
 		}
 	}
 
+	/// Target with all extensions present
+	pub fn all() -> Self {
+		let mut target = Self::new();
+		target.add_extension(Extension::IntegerMultplication);
+		target
+	}
+
 	pub fn parse(s: &str) -> Option<Self> {
 		let mut target = Self::new();
 
 		// Need at least `bibe32`
-		if s.len() < 6 || &s[0..5] != "bibe32" {
+		if s.len() < 6 || &s[0..6] != "bibe32" {
 			return None;
 		}
 
 		for c in (&s[6..]).chars() {
 			match c {
+				'A' => return Some(Self::all()),
 				'i' => target.add_extension(Extension::IntegerMultplication),
 				_ => continue,
 			}
@@ -52,8 +61,23 @@ impl Target {
 	pub fn add_extension(&mut self, extension: Extension) {
 		self.extensions.insert(extension);
 	}
+}
 
-	pub fn is_64bit(&self) -> bool {
-		false
+#[cfg(test)]
+mod test {
+	use super::*;
+
+	#[test]
+	fn test_parse() {
+		// Try no extensions
+		assert!(Target::parse("bibe32").is_some());
+
+		// Try all the individual ones
+		assert!(Target::parse("bibe32i").is_some());
+		assert!(Target::parse("bibe32A").is_some());
+
+		// Verify that the target string has to start with 'bibe32'
+		assert!(Target::parse("i").is_none());
+		assert!(Target::parse("A").is_none());
 	}
 }
